d93ec2c673b8dac39c1cc9e38a1d480c94e51aae
Aaron <2233692@slu.edu.ph>
2024-04-23 21:13:01 +0800

    +created the left panel including its buttons

--- src/client_java/view/ClientSettingsView.java
+++ src/client_java/view/ClientSettingsView.java
@@ -1,4 +1,4 @@
-package server.view;
+package client_java.view;
 
 import shared.SwingStylesheet;
 
@@ -6,32 +6,37 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
-public class SecuritySettingsView extends JFrame{
+public class ClientSettingsView extends JFrame{
     private JLabel lblNavLocation; // the label showing the current page of the application
     private JButton btnNavHome; // the home navigation button
     private JButton btnNavSettings; // the settings navigation button
     private JButton btnNavLogout; // the logout navigation button
+    private JButton leftPnlButtons; // the left panel's buttons with labels
+    private JButton btnAccMa; // the manage account button
+    private JButton btnDelAcc; // the delete account button
+    private JButton btnMusic; // the music toggle button
     private JPanel pnlCards; // the panel holding other view components
     private CardLayout cardLayout; // the card layout that controls other components
     private SwingStylesheet style = new SwingStylesheet(); // the stylesheet
 
 
-    // constructs a frame of SecuritySettingsView
-    public SecuritySettingsView() {
+    // constructs a frame of ClientSettingsView
+    public ClientSettingsView() {
         super("Word Factory");
 
         Container contentArea = new JPanel(new BorderLayout());
         contentArea.setBackground(style.white);
 
-        contentArea.add(new server.view.SecuritySettingsView.HeaderPanel(), BorderLayout.NORTH);
+        contentArea.add(new ClientSettingsView.HeaderPanel(), BorderLayout.NORTH);
 
-        contentArea.add(new server.view.SecuritySettingsView.LeftPanel(), BorderLayout.WEST);
+        contentArea.add(new ClientSettingsView.LeftPanel(), BorderLayout.WEST);
 
         pnlCards = new JPanel(cardLayout = new CardLayout(0,0));
+        pnlCards.setBackground(style.deepSkyBlue);
         contentArea.add(pnlCards, BorderLayout.CENTER);
 
         /**
-         * Placeholder for calling a future view for the security settingsView
+         * Placeholder for calling a future view for the settings view
          *  //pnlCards.add(new DefaultGameSettingsView(), "defaultSettings");
          *  //cardLayout.show(pnlCards, "defaultSettings");
          */
@@ -81,15 +86,43 @@ public class SecuritySettingsView extends JFrame{
         }
     }
 
-    class LeftPanel extends JPanel{
+    class LeftPanel extends JPanel {
         // constructs a panel of LeftPanel
-        public LeftPanel(){
+        public LeftPanel() {
             this.setBackground(style.mustard);
 
-            JPanel container = new JPanel(new GridLayout(0,2,180,0));
+            JPanel container = new JPanel();
+            container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
             container.setBackground(style.mustard);
             container.setBorder(style.padding);
             add(container);
+
+            btnAccMa = navButtonWithLabel(container, style.iconAccMan, "Account Management");
+            btnDelAcc = navButtonWithLabel(container, style.iconAccMan, "Delete Account");
+            btnMusic = navButtonWithLabel(container, style.iconMusic, "Music:");
+
+            container.setPreferredSize(new Dimension(250, 300));
+
+            container.setBorder(BorderFactory.createEmptyBorder(0, -50, 0, 0));
+        }
+
+        private JButton navButtonWithLabel(JPanel container, Icon icon, String labelText) {
+            JPanel panel = new JPanel(new GridLayout(1, 2, -50, 0));
+            panel.setBackground(style.mustard);
+
+            leftPnlButtons = style.createBtnIconOnly((ImageIcon) icon, 30, 30);
+            leftPnlButtons.setBorder(BorderFactory.createEmptyBorder(20, -10, 20, -10));
+
+            JLabel label = new JLabel(labelText);
+            label.setFont(new Font("Arial", Font.BOLD, 16));
+            label.setForeground(style.white);
+            label.setBorder(BorderFactory.createEmptyBorder(20, 1, 20, -10));
+
+            panel.add(leftPnlButtons);
+            panel.add(label);
+
+            container.add(panel);
+            return null;
         }
     }
 
@@ -111,6 +144,21 @@ public class SecuritySettingsView extends JFrame{
     // retrieves the current JButton of btnNavLogout
     public JButton getBtnNavLogout() {
         return btnNavLogout;
+    }
+
+    //retrieves the current JButton of btnAccMa
+    public JButton getBtnAccMa(){
+        return btnAccMa;
+    }
+
+    //retrieves the current JButton of btnAccMa
+    public JButton getBtnDelAcc(){
+        return btnDelAcc;
+    }
+
+    //retrieves the current JButton of btnAccMa
+    public JButton getBtnMusic(){
+        return btnMusic;
     }
 
     // retrieves the current JPanel of pnlCards
@@ -138,12 +186,27 @@ public class SecuritySettingsView extends JFrame{
         btnNavLogout.addActionListener(actionListener);
     }
 
+    // sets a specified action listener for btnAccMa
+    public void setAccountManagementListener(ActionListener actionListener) {
+        btnAccMa.addActionListener(actionListener);
+    }
+
+    // sets a specified action listener for btnDelAcc
+    public void setDeleteAccountListener(ActionListener actionListener) {
+        btnDelAcc.addActionListener(actionListener);
+    }
+
+    // sets a specified action listener for btnMusic
+    public void setMusicListener(ActionListener actionListener) {
+        btnMusic.addActionListener(actionListener);
+    }
+
     // sets a specified text for lblNavLocation
     public void setLocationText(String text) {
         lblNavLocation.setText(text);
     }
 
     public static void main(String[] args) {
-        new server.view.SecuritySettingsView();
+        new ClientSettingsView();
     }
 }
