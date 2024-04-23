4ae65a2c4c317e97862bb0489841ba9fc52411a4
MARVIN PATRICK LACANILAO <2230153@slu.edu.ph>
2024-04-24 01:43:17 +0800

    Revisions for PlayersView

--- src/server/view/subpages/PlayersView.java
+++ src/server/view/subpages/PlayersView.java
@@ -18,8 +18,9 @@ public class PlayersView extends JPanel {
     private SwingStylesheet style = new SwingStylesheet();
 
     public PlayersView() {
-        this.setLayout(new BorderLayout());
-        this.setPreferredSize(new Dimension(1300, 750));
+        setBackground(style.deepSkyBlue);
+        setLayout(new BorderLayout());
+        setPreferredSize(new Dimension(1300, 750));
 
         FunctionPanel functionPanel = new FunctionPanel();
         TablePanel tablePanel = new TablePanel();
@@ -27,12 +28,12 @@ public class PlayersView extends JPanel {
 
         pnlButtons = new JPanel();
         btnRemove = style.createBtnRounded("REMOVE", style.red,style.white,10);
-        btnCancel = style.createBtnRounded("CANCEL", style.deepSkyBlue,style.white,10);
+        btnCancel = style.createBtnRounded("CANCEL", style.goldenTainoi,style.white,10);
 
         // Add the buttons to pnlButtons
         pnlButtons.setLayout(new FlowLayout());
         pnlButtons.setPreferredSize(new Dimension(1100, 70));
-        pnlButtons.setBackground(style.lightGray);
+        pnlButtons.setBackground(style.deepSkyBlue);
         pnlButtons.add(btnRemove);
         pnlButtons.add(btnCancel);
 
@@ -48,20 +49,21 @@ public class PlayersView extends JPanel {
 
         private JTextField txtSearchbar;
         private JButton btnSearch;
+        private JButton btnAdd;
         private JButton btnAddPlayer;
 
         public FunctionPanel() {
-            this.setPreferredSize(new Dimension(1300, 70));
+            setBackground(style.deepSkyBlue);
+            setPreferredSize(new Dimension(1300, 70));
 
-            JPanel pnlFunction = style.createPnlRounded(1200, 50, style.goldenTainoi, style.lightGray);
+            JPanel pnlFunction = style.createPnlRounded(1200, 50, style.white, style.deepSkyBlue);
             pnlFunction.setLayout(new BorderLayout());
             pnlFunction.setBorder(style.padding);
             add(pnlFunction, BorderLayout.CENTER);
 
             // Search panel
             JPanel pnlSearch = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 0));
-            pnlSearch.setBackground(style.goldenTainoi);
-            pnlSearch.setBorder(new EmptyBorder(0, 0, 0, 50));
+            pnlSearch.setBackground(style.white);
             pnlFunction.add(pnlSearch, BorderLayout.WEST);
 
             txtSearchbar = style.createTxtRounded("Search Players", style.lightGray, style.gray, 20);
@@ -70,14 +72,16 @@ public class PlayersView extends JPanel {
             btnSearch = style.createBtnIconOnly(style.iconSearch, 25, 25);
             pnlSearch.add(btnSearch);
 
-            JPanel pnlButton = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 0));
-            pnlButton.setBackground(style.goldenTainoi);
-            pnlButton.setBorder(new EmptyBorder(0, 0, 0, 50));
-            pnlSearch.add(pnlButton, BorderLayout.EAST);
+            // Button panel
+            JPanel pnlButton = new JPanel(new FlowLayout(FlowLayout.RIGHT, 5, 0));
+            pnlButton.setBackground(style.white);
+            pnlFunction.add(pnlButton, BorderLayout.EAST);
+
+            btnAdd = style.createBtnIconOnly(style.iconAdd, 25, 25);
+            pnlButton.add(btnAdd);
 
             btnAddPlayer = style.createBtnTxtOnly("ADD PLAYER", style.deepSkyBlue);
-//            btnAddPlayer.setEnabled(false);
-            pnlFunction.add(btnAddPlayer, BorderLayout.EAST);
+            pnlButton.add(btnAddPlayer);
         }
     }
 
@@ -90,19 +94,29 @@ public class PlayersView extends JPanel {
         private Font headerFont;
 
         public TablePanel() {
-            this.setPreferredSize(new Dimension(1300, 600));
+            setBackground(style.deepSkyBlue);
+            setPreferredSize(new Dimension(1300, 600));
 
-            pnlTable = style.createPnlRounded(1200, 550, style.goldenTainoi, style.white);
+            pnlTable = style.createPnlRounded(1200, 550, style.white, style.white);
             pnlTable.setBorder(style.padding);
             pnlTable.setLayout(new BorderLayout());
             add(pnlTable, BorderLayout.CENTER);
 
             tblPlayersModel = new DefaultTableModel();
+
             tblPlayersModel.addColumn("");
             tblPlayersModel.addColumn("USERNAME");
             tblPlayersModel.addColumn("FULL NAME");
 
             tblPlayers = new JTable(tblPlayersModel);
+
+            tblPlayers.setDragEnabled(false);
+            tblPlayers.setOpaque(false);
+            tblPlayers.setFillsViewportHeight(true);
+            tblPlayers.getTableHeader().setResizingAllowed(false);
+            tblPlayers.getTableHeader().setReorderingAllowed(false);
+            tblPlayers.getTableHeader().setFont(style.bowlbyOne.deriveFont(Font.BOLD, 14));
+
             tblPlayers.getColumnModel().getColumn(0).setPreferredWidth(200);
             tblPlayers.getColumnModel().getColumn(1).setPreferredWidth(350);
             tblPlayers.getColumnModel().getColumn(2).setPreferredWidth(350);
@@ -111,27 +125,24 @@ public class PlayersView extends JPanel {
             tblPlayers.setRowHeight(30);
             tblPlayers.setRowHeight(30);
 
-            headerFont = tblPlayers.getTableHeader().getFont();
-            tblPlayers.getTableHeader().setFont(headerFont.deriveFont(Font.BOLD, 14));
-
             tblPlayers.setBackground(style.mustard);
 
             JScrollPane scrollPane = new JScrollPane(tblPlayers);
-            scrollPane.setPreferredSize(new Dimension(1200, 520));
+            scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
             pnlTable.add(scrollPane, BorderLayout.CENTER);
         }
     }
 
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Players View");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.getContentPane().add(new PlayersView());
-            frame.pack();
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
-        });
-    }
+    //TO DO: Delete after testing JDialogs
+//    public static void main(String[] args) {
+//        SwingUtilities.invokeLater(() -> {
+//            JFrame frame = new JFrame("Players View");
+//            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//            frame.getContentPane().add(new PlayersView());
+//            frame.pack();
+//            frame.setLocationRelativeTo(null);
+//            frame.setVisible(true);
+//        });
+//    }
 }
 
