b5b572f72775857b19303750613538f4418737b0
RAMON EMMIEL JASMIN <2230748@slu.edu.ph>
2024-04-24 19:03:41 +0800

    Added the getMatches and getWins implementation

--- src/client/model/subpages/SettingsModel.java
+++ src/client/model/subpages/SettingsModel.java
@@ -12,13 +12,35 @@ public class SettingsModel {
         this.wfImpl = wfImpl;
     }
 
+    /**
+     * Returns the username of the player
+     * @return
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Returns the BoggleClient object
+     * @return
+     */
     public BoggleClient getWfImpl() {
         return wfImpl;
     }
 
+    /**
+     * Returns the total matches of the player
+     * @return
+     */
+    public int getMatches(){
+        return wfImpl.getMatches(this.username);
+    }
 
+    /**
+     * Returns the total wins of the player
+     * @return
+     */
+    public int getWins(){
+        return wfImpl.getWins(this.username);
+    }
 }
