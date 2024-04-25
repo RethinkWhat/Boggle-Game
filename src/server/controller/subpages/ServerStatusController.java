fc223b2e2429a9138bf9698716747cccee71f72c
RAMON EMMIEL JASMIN <2230748@slu.edu.ph>
2024-04-25 09:52:37 +0800

    Added ServerStatusController

---
+++ src/server/controller/subpages/ServerStatusController.java
@@ -0,0 +1,50 @@
+package server.controller.subpages;
+
+import org.omg.CORBA.ORB;
+import org.omg.CosNaming.NameComponent;
+import org.omg.CosNaming.NamingContextExt;
+import org.omg.CosNaming.NamingContextExtHelper;
+import org.omg.PortableServer.POA;
+import org.omg.PortableServer.POAHelper;
+import server.controller.Server;
+import server.model.BoggleApp.BoggleClient;
+import server.model.BoggleApp.BoggleClientHelper;
+import server.model.ServerImplementation;
+import server.model.subpages.ServerStatusModel;
+import server.view.subpages.ServerStatusView;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ServerStatusController {
+    private ServerStatusView view;
+    private ServerStatusModel  model;
+
+    private Server server = new Server();
+
+    private String[] args = {"-ORBINITIALHOST", "localhost", "-ORBINITIALPORT", "5000"};
+
+    public ServerStatusController(ServerStatusView view, ServerStatusModel model) {
+        this.view = view;
+        this.model = model;
+
+        view.setServerListener(new ServerSwitchListener());
+    }
+
+    class ServerSwitchListener implements ActionListener{
+
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            if (view.getServerStatus().getText().equals("OFFLINE")){
+                view.setOnline();
+                server.run(args);
+            }else {
+                view.setOffline();
+                server.stop();
+            }
+        }
+    }
+
+}
