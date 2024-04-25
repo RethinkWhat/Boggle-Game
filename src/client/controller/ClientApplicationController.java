9c7d606ef90ce2be7f1fb8d406fbcdf8d79df68d
JOHAN RICKARDO ROXAS <2233375@slu.edu.ph>
2024-04-25 21:58:08 +0800

    Added intitial music

--- src/client/controller/ClientApplicationController.java
+++ src/client/controller/ClientApplicationController.java
@@ -4,11 +4,15 @@ import client.controller.subpages.LobbyController;
 import client.model.ClientApplicationModel;
 import client.model.subpages.LobbyModel;
 import client.view.ClientApplicationView;
-import client.view.subpages.LobbyView;
+import shared.SwingResources;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class ClientApplicationController {
     /**
@@ -20,68 +24,43 @@ public class ClientApplicationController {
      * The model
      */
     private ClientApplicationModel model;
-
+    /**
+     * The audio input stream for music.
+     */
+    private AudioInputStream audioMusicStream;
+    /**
+     * The audio input stream for sfx.
+     */
+    private AudioInputStream audioSoundStream;
 
     /**
      * Constructs a ClientApplicationController with a specified view.
-     *
      * @param view The specified view.
      */
     public ClientApplicationController(ClientApplicationView view, ClientApplicationModel model) {
         this.model = model;
         this.view = view;
 
+        initializeMusic();
+
         // action listeners
         this.view.getHomeView().setJoinListener(new JoinGameListener());
-        this.view.setSettingsListener(new SettingsListener());
-        this.view.setHomeListener(new HomeListener());
-        this.view.setLogoutListener(new LogOutListener());
 
         // mouse listeners
+        view.getBtnNavHome().addMouseListener(new SwingResources.CursorChanger(view.getBtnNavHome()));
+        view.getBtnNavSettings().addMouseListener(new SwingResources.CursorChanger(view.getBtnNavSettings()));
+        view.getBtnNavLogout().addMouseListener(new SwingResources.CursorChanger(view.getBtnNavLogout()));
+
+
     }
 
-    public class SettingsListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            // Switch to the settings view
-            view.showSettings();
-            view.setNavLocationText("Settings");
-        }
-    }
-
-    public class HomeListener implements ActionListener {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            view.showHome();
-            view.setNavLocationText("Home");
-        }
-    }
-    public class LogOutListener implements ActionListener {
-        //TEMPORARY PROMPT FOR LOGOUT
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            int choice = JOptionPane.showConfirmDialog(
-                    view,
-                    "Are you sure you want to log out?",
-                    "TEMPORARY PROMPT",
-                    JOptionPane.YES_NO_OPTION
-            );
-
-            if (choice == JOptionPane.YES_OPTION) {
-                view.dispose();
-            }
-        }
-    }
     public class JoinGameListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            view.showLobby();
-            view.setNavLocationText("Lobby");
-            System.out.println("creating new lobby");
-
-            new LobbyController(new LobbyModel(model.getUsername(), model.getWfImpl()), view.getLobbyView(), view);
-
-            view.getLobbyView().setExitLobbyListener(new ActionListener() {
+            SwingUtilities.invokeLater(() -> {
+                view.showLobby();
+                view.setNavLocationText("Lobby");System.out.println("creating new lobby");
+                new LobbyController(new LobbyModel(model.getUsername(), model.getWfImpl()), view.getLobbyView(), view);view.getLobbyView().setExitLobbyListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
 //                    model.getWfImpl().exitGameRoom(model.getUsername());
@@ -91,23 +70,35 @@ public class ClientApplicationController {
                 }
             });
             // Add action listener to the exit lobby button
+                /*
+                BooleanHolder startGame = new BooleanHolder(false);
+                long id;
+                while (!startGame.value) {
+                    id = model.getWfImpl().attemptJoin("username", startGame);
+                    System.out.println("TIME REMAINING: " + id);
+                    if (startGame.value) {
 
-            /*
-            BooleanHolder startGame = new BooleanHolder(false);
-            long id;
-            while (!startGame.value) {
-                id = model.getWfImpl().attemptJoin("username", startGame);
-                System.out.println("TIME REMAINING: " + id);
-                if (startGame.value) {
+                        break;
+                    }try {
+                        Thread.sleep(1000);
+                    } catch (Exception p) {
+                    p.printStackTrace();}
+                }
+                */
+            });
+        }
+    }
 
-                    break;
-                }try {
-                    Thread.sleep(1000);
-                } catch (Exception p) {
-                p.printStackTrace();}
-            }
-
-             */
+    public void initializeMusic() {
+        String musicPath = "res/audio/music/lobby-music-v2.wav";
+        try {
+            audioMusicStream = AudioSystem.getAudioInputStream(new File(musicPath).getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioMusicStream);
+            clip.start();
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
