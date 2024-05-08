89ed003b0e2dad7d3f0c8e082c47c514e57c4310
SEBASTIAN SICCUAN <2233205@slu.edu.ph>
2024-05-08 12:55:39 +0000

    Update Server.java

--- src/server/controller/Server.java
+++ src/server/controller/Server.java
@@ -43,7 +43,7 @@ public class Server{
         try {
             Properties props = new Properties();
             props.put("org.omg.CORBA.ORBInitialPort","1500");//change as needed
-            props.put("org.omg.CORBA.ORBInitialHost", "192.168.1.2");//change as needed
+            props.put("org.omg.CORBA.ORBInitialHost", "x.x.x.x");//change as needed
             this.orb = ORB.init(args, props);
 
 //            this.orb = ORB.init(args, null);
