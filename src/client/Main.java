1b287e7c7d500e089021dfc75597c5b575459812
Sebastian Siccuan <basti@Cherry-Mobile-Laptop.local>
2024-05-08 20:45:03 +0800

    Fixed Client connectivity

--- src/client/Main.java
+++ src/client/Main.java
@@ -9,8 +9,9 @@ public class Main {
 
     public static void main(String[] var0) {
         SwingStylesheet.loadFonts();
-        String[] args = {"-ORBINITIALHOST", "localhost", "-ORBINITIALPORT", "5000"};
-        (new Client()).run(args);
+//      String[] args = {"-ORBInitialPort", "1500", "-ORBInitialHost", "192.168.1.2"};
+        //edit Configuration before running
+        (new Client()).run(var0);
 
     }
 }
