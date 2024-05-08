b5c269777aa0793d2d2af2634c9d1d879cb1d347
SEBASTIAN SICCUAN <2233205@slu.edu.ph>
2024-05-08 12:57:29 +0000

    Update ServerApplicationController.java

--- src/server/controller/ServerApplicationController.java
+++ src/server/controller/ServerApplicationController.java
@@ -22,7 +22,7 @@ public class ServerApplicationController {
     private PlayersController playersController;
 
     private Server server;
-    private String[] args = {"-ORBInitialPort", "1500", "-ORBInitialHost", "192.168.1.2"};//change as needed
+    private String[] args = {"-ORBInitialPort", "1500", "-ORBInitialHost", "localhost"};//change as needed
 
     public ServerApplicationController(ServerApplicationView view, ServerApplicationModel model) {
         this.view = view;
